91c165cdb43a7fa948980a7bee7aec95ccf34ffc
abrahamga <abraham.garcia@taxsat.com>
2020-07-17 14:26:53 -0500

    repos

--- js/git.js
+++ js/git.js
@@ -1,9 +1,28 @@
-'use strict'
+
 
 const url = "https://api.github.com/users/DevGar1/repos";
 
 function git() {
-    const git = document.getElementById('git');
-
+    const git = document.getElementById('projects');
+    const prueba = document.createElement('div');
+    console.log(git);
+    prueba.innerText = 'dksalfjs';
+    git.appendChild(prueba);
+    // let names = [];
+    // fetch(url)
+    //     .then(res => res.json()
+    //         .then(data => {
+    //             names = data;
+    //             const fragment = document.createDocumentFragment();
+    //
+    //             for (const name of names) {
+    //                 const div = document.createElement('div');
+    //                 div.textContent = name.name;
+    //                 git.appendChild(div);
+    //             }
+    //
+    //
+    //         })
+    //     );
 
 }
