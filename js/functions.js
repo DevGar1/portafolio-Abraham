feded2af63402cec0f51f1fccd10b152dddbecf5
abrahamga <abraham.garcia@taxsat.com>
2020-07-18 16:25:09 -0500

    mobile

--- js/functions.js
+++ js/functions.js
@@ -6,3 +6,16 @@ function fun() {
    img.style.transition = 'all 1s';
    img.style.transform = "rotateY(360deg)";
 }
+
+function bar() {
+   const menu = document.getElementById('menu');
+   if (menu.style.display === 'none'){
+      menu.style.display = 'flex';
+      menu.style.justifyContent = 'space-between';
+      menu.style.transition = 'all 1s';
+   } else {
+      menu.style.transition = 'all 1s';
+      menu.style.display = 'none';
+   }
+
+}
