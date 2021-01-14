f6cc8736ac8a068a7a021ff39f449078a405a579
AbeGar13 <ga.abraham00@gmail.com>
2021-01-13 20:06:55 -0600

    update

--- js/git.js
+++ js/git.js
@@ -3,6 +3,30 @@
 const url = "https://api.github.com/users/DevGar1/repos";
 const gif = [
     {
+        "path": "im/dulceria.jpg",
+        "description": "Proyecto creado para dar una explicación sencilla sobre flex-box"
+    },{
+        "path": "im/compu.jpeg",
+        "description": "Proyecto creado para dar una explicación sencilla sobre flex-box"
+    },{
+        "path": "im/dul.jpg",
+        "description": "Proyecto creado para dar una explicación sencilla sobre flex-box"
+    },{
+        "path": "im/ferreteria .jpg",
+        "description": "Proyecto creado para dar una explicación sencilla sobre flex-box"
+    },{
+        "path": "im/aling-end.gif",
+        "description": "Proyecto creado para dar una explicación sencilla sobre flex-box"
+    },{
+        "path": "im/nest-1.png",
+        "description": "Proyecto creado para dar una explicación sencilla sobre flex-box"
+    },{
+        "path": "im/aling-end.gif",
+        "description": "Proyecto creado para dar una explicación sencilla sobre flex-box"
+    },{
+        "path": "im/aling-end.gif",
+        "description": "Proyecto creado para dar una explicación sencilla sobre flex-box"
+    },{
         "path": "im/aling-end.gif",
         "description": "Proyecto creado para dar una explicación sencilla sobre flex-box"
     }
@@ -16,6 +40,7 @@ function git() {
         .then(res => res.json()
             .then(repos => {
                 for (const repo of repos) {
+                    console.log(repo);
                     if (repo.name !== 'portafolio-Abraham') {
                         const div = document.createElement('div');
                         div.style.padding = '3%';
@@ -24,11 +49,11 @@ function git() {
                         h4.style.marginTop = '13px';
                         div.appendChild(h4);
                         const p = document.createElement('p');
-                        p.textContent = gif[index].description;
+                        p.textContent = repo.description;
                         div.appendChild(p);
                         const img = document.createElement('img');
                         img.src = gif[index].path;
-                        img.alt = gif[index].description;
+                        img.alt = repo.description;
                         div.appendChild(img);
                         const a = document.createElement('a');
                         a.href = repo.html_url;
